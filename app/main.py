0730bda3a31e85f1991cbc0fe442552062aecd63
Diego <diegomaneyro@gmail.com>
2023-03-29 06:52:57 -0300

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -2,62 +2,48 @@ import streamlit as st
 import os
 import yfinance as yf
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import seaborn as sns
 from datetime import datetime, timedelta
-import matplotlib.dates as mdates
-
-
+st.set_option('deprecation.showPyplotGlobalUse', False)
+st.title('Analytics-app')
 st.markdown('***')
-st.title('Analytics app')
-st.markdown('***')
+st.sidebar.markdown('* Impacto del COVID-19 en el mercado financiero y en la lista índice S&P500')
+st.sidebar.markdown('* EDA Indice SP-500')
+st.sidebar.markdown('* Análisis de riesgo')
+st.sidebar.markdown('* Análisis de crecimiento')
+st.sidebar.markdown('* Recomendación de inversion: \n Empresa y dia')
 
-st.sidebar.markdown('Impacto del COVID-19 en el mercado financiero y en la lista índice S&P500')
-st.sidebar.markdown('***')
-st.sidebar.markdown('Análisis de riesgo')
-st.sidebar.markdown('***')
-st.sidebar.markdown('Análisis de crecimiento')
 st.markdown('''
- Es cierto que la pandemia del COVID-19 tuvo un impacto significativo en los mercados financieros en todo el mundo,
+>La pandemia del COVID-19 tuvo un impacto significativo en los mercados financieros en todo el mundo,
 incluyendo la lista índice S&P500. En particular, a principios de 2020, el mercado sufrió una gran caída
-en el precio de las acciones debido a la incertidumbre causada por la pandemia
+en el precio de las acciones debido a la incertidumbre causada por la pandemia.
 ''')
 st.markdown('''
- Si comparamos el comportamiento del índice S&P500 en los años previos al COVID-19 con el comportamiento 
+>Si comparamos el comportamiento del índice S&P500 en los años previos al COVID-19 con el comportamiento 
 durante la pandemia, podemos observar una diferencia significativa. Antes de la pandemia, 
 el índice S&P500 mostraba una tendencia alcista estable, con fluctuaciones normales en el corto plazo.
  Sin embargo, a partir de marzo de 2020, el índice experimentó una fuerte caída, 
 seguida de una recuperación rápida y volátil.
 ''')
 st.markdown('***')
+st.subheader('Análisis del Indice SP-500')
 
-# Descargar datos históricos de Apple y el S&P 500
-apple = yf.download("AAPL", start="2000-01-01")
-sp500 = yf.download("^GSPC", start="2000-01-01")
-
-# Fusionar los datos de Apple y el S&P 500 en un solo DataFrame
-data = pd.merge(apple['Close'], sp500['Close'], left_index=True, right_index=True, suffixes=('_apple', '_sp500'))
-
-# Calcular la desviación estándar de los precios de cierre de Apple en relación al S&P 500
-std_dev = data['Close_apple'].std() / data['Close_sp500'].std()
-
-# Graficar los precios de cierre de Apple y el S&P 500 usando Matplotlib
-fig, ax = plt.subplots()
-ax.plot(data['Close_apple'], label='Apple')
-ax.plot(data['Close_sp500'], label='S&P 500')
-ax.legend()
-st.pyplot(fig)
-
-# Imprimir la desviación estándar de Apple en relación al S&P 500
-st.write(f"La desviación estándar de los precios de cierre de Apple en relación al S&P 500 es {std_dev:.2f}")
-
-
-
-
-
-
-
-
+# ingestar datos desde csv en un dataframe de pandas
+sp500_data = pd.read_csv('sp500_historical_data.csv')
+# crear un check para seleccionar filas o columnas y luego se visualiza los datos del csv
+st.subheader('Información sp500: datos históricos posterior al año 2000') 
+if st.checkbox('Mostrar SP-500 Historico'):
+    st.dataframe(sp500_data)
+# Visualizacion inicial de los primeros registros
+st.dataframe(sp500_data.describe())
 
 st.markdown('***')
-st.markdown('contacto')
+sp500_empresas = pd.read_csv('sp500_empresas.csv')
+st.subheader('Información de Microsoft, Apple, Google, Amazon')
+if st.checkbox('Mostrar SP-500 Empresas'):
+    st.dataframe(sp500_empresas)
+st.dataframe(sp500_empresas.describe())
+st.markdown('***')
